a562dd14f6479319a65df51a36bde7e5ccf429e7
Brian Cabrera <briancabrer4@gmail.com>
2022-01-19 19:30:55 -0300

    contact form fully fuctional, animation added onSubmit

--- client/src/Components/Contact/Form/Form.jsx
+++ client/src/Components/Contact/Form/Form.jsx
@@ -1,36 +1,62 @@
-import React from 'react'
+import { useState } from 'react'
 import { useForm } from "react-hook-form"
 import "./form.scss"
+import emailjs from "emailjs-com"
 
 export default function Form() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [sent, setSent] = useState(false);
+
+    
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_hthxlum', e.target, 'user_0TmhV8cDn0F7e7HrEAd9p')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+        setSent(true);
+    }
 
     return (
         <div className="form">
-            <div className="background">
-                <img src={require("../../resources/contact/contactform.png")} alt="" />
-            </div>
-            <div className="form-container">
-                <form onSubmit={handleSubmit()}>
-                    <div className="form-group">
-                        <label>Name</label>
-                        <input type="text" />
+            {
+                !sent ?
+                <>
+                    <div className="background">
+                        <img src={require("../../resources/contact/contactform.png")} alt="" />
                     </div>
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input type="text" />
+                    <div className="form-container">
+                        <form onSubmit={sendEmail}>
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input className="field" type="text" name="name"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input className="field" type="text" name="email"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Subject</label>
+                                <input className="field" type="text" name="subject"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Message</label>
+                                <textarea className="field" id="message" name="message" />
+                            </div>
+                            <input type="submit" id="submit-btn" value="Send"/>
+                        </form>
                     </div>
-                    <div className="form-group">
-                        <label>Subject</label>
-                        <input type="text" />
+                </>
+                :
+                <>
+                    <div className="background">
+                        <img src={require("../../resources/contact/animatedform.gif")} alt="" />
                     </div>
-                    <div className="form-group">
-                        <label>Message</label>
-                        <input type="text" id="message" />
-                    </div>
-                    <input type="submit" id="submit-btn" value="Send"/>
-                </form>
-            </div>
+                </>
+            }
         </div>
     )
 }
