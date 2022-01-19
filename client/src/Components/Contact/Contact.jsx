d8ae9fddfc7d9054d3dfd15262bed3d5b8f1da5d
Brian Cabrera <briancabrer4@gmail.com>
2022-01-19 16:30:01 -0300

    created contact form, still needs functionality

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import "./contact.scss"
 import Form from "./Form/Form.jsx"
 
 export default function Contact() {
+
     return (
-        <div>
-            
+        <div className="contact">
+            <Link to="/home" className="link">
+                <span>
+                    Home
+                </span>
+            </Link>
+            <Form />
         </div>
     )
 }
--- client/src/Components/Contact/Form/Form.jsx
+++ client/src/Components/Contact/Form/Form.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
+import "./form.scss"
 
 export default function Form() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
     return (
-        <div>
-            
+        <div className="form">
+            <div className="background">
+                <img src={require("../../resources/contact/contactform.png")} alt="" />
+            </div>
+            <div className="form-container">
+                <form onSubmit={handleSubmit()}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" />
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="text" />
+                    </div>
+                    <div className="form-group">
+                        <label>Subject</label>
+                        <input type="text" />
+                    </div>
+                    <div className="form-group">
+                        <label>Message</label>
+                        <input type="text" id="message" />
+                    </div>
+                    <input type="submit" id="submit-btn" value="Send"/>
+                </form>
+            </div>
         </div>
     )
 }
