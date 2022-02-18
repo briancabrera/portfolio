1fe863aea7093a71c663c5d4707838cd92087060
Brian Cabrera <briancabrer4@gmail.com>
2022-02-18 20:01:56 -0300

    frontend asteroid game folder structure

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -28,7 +28,13 @@ export default function Home() {
                             Resume
                         </span>
                     </a>
-                
+
+                <Link to="/game" className="animated link">
+                    <span className="btn" 
+                    onMouseEnter={() => setAbout(true)} onMouseLeave={() => setAbout(false)}>
+                        About me
+                    </span>
+                </Link>
             </div>
             <div className="animated room">
                 <img src={require("../resources/house/inside/inside.png")} alt="" id={about || projects || contact || resume? "roomhidden" : "room"}/>
